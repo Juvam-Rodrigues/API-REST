93019194b1d6e0e62d067fde2d1771cc02abea1b
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2026-06-05 11:48:02 -0300

    Alteração de importação de Document.

--- apirest/src/main/java/br/ufrn/bdnosql/apirest/controller/DocumentoController.java
+++ apirest/src/main/java/br/ufrn/bdnosql/apirest/controller/DocumentoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.bson.Document;
 
 import br.ufrn.bdnosql.apirest.message.CustomMessage;
 import br.ufrn.bdnosql.apirest.service.DocumentoService;
